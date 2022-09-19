ceb23b3a3045faa49109628fcb11458f2230371d
E-YAN <48629008+E-YAN@users.noreply.github.com>
2022-09-19 13:13:38 +0300

    Subscribers

---
+++ migrations/20220919095459-create-subscriber.js
@@ -0,0 +1,35 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('subscribers', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      email: {
+        unique: true,
+        type: Sequelize.STRING
+      },
+      no: {
+        unique: true,
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deletedAt: {
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('subscribers');
+  }
+};
