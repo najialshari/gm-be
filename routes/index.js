6cb96d7ad4b8078597112c8df49fd88abe4cb986
najialshari <najigoon2@gmail.com>
2022-09-25 11:57:08 +0300

    Testing APIs with Postman tool

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,11 @@ var router = express.Router();
 var usersRouter = require('../lib/users/routes');
 var menusRouter = require('../lib/menu/routes');
 // var qrCodesRouter = require('../lib/qrCodes/routes');
-// var ordersRouter = require('../lib/orders/routes');
+var ordersRouter = require('../lib/orders/routes');
 
 router.use('/users', usersRouter);
 // app.use('/qrCodes', qrCodesRouter);
-// app.use('/orders', ordersRouter);
+router.use('/orders', ordersRouter);
 router.use('/menus', menusRouter);
 
 module.exports = router;
