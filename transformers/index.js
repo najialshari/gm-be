c91c5897596c2a7170642fa5dc52151d38359c4d
najialshari <najigoon2@gmail.com>
2022-09-27 08:28:53 +0300

    OrdersUpdate

---
+++ transformers/index.js
@@ -0,0 +1,29 @@
+const userTransformer = (user) => {
+  delete user["dataValues"]["password"];
+  delete user["dataValues"]["deletedAt"];
+  delete user["dataValues"]["createdAt"];
+  delete user["dataValues"]["updatedAt"];
+  return user;
+};
+
+
+// const linksTransformers = (links) => {
+//   links.map((link) => {
+//     delete link["dataValues"]["qrlinks"];
+//     delete link["dataValues"]["userId"];
+//     delete link["dataValues"]["isPrivate"];
+//     delete link["dataValues"]["order"];
+//     delete link["dataValues"]["deletedAt"];
+//     delete link["dataValues"]["createdAt"];
+//     delete link["dataValues"]["updatedAt"];
+//     delete link["dataValues"]["linktype"]['createdAt'];
+//     delete link["dataValues"]["linktype"]['updatedAt'];
+//   });
+//   return links;
+// };
+
+
+
+module.exports = {
+  userTransformer
+};
