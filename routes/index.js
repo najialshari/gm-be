c12028a82df2c274a51538757b804e7931af4d78
najialshari <najigoon2@gmail.com>
2022-09-29 16:59:45 +0300

    UsersQrCode

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,15 @@ var express = require('express');
 var router = express.Router();
 var usersRouter = require('../lib/users/routes');
 var menusRouter = require('../lib/menu/routes');
-// var qrCodesRouter = require('../lib/qrCodes/routes');
+var qrCodesRouter = require('../lib/qrCodes/routes');
 var ordersRouter = require('../lib/orders/routes');
 var subscriptionRouter = require('../lib/subscribers/routes');
 
+router.get('/', (req, res, next) => {
+    res.send("Welcome to back-end home page")
+});
 router.use('/users', usersRouter);
-// app.use('/qrCodes', qrCodesRouter);
+router.use('/qrCodes', qrCodesRouter);
 router.use('/orders', ordersRouter);
 router.use('/menus', menusRouter);
 router.use('/subscription', subscriptionRouter);
