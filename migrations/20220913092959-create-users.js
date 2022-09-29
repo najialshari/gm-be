318e32e60922ca03abf8645a64e558b2a94f6e18
YAHYA SHAMERI <yahyanageeb@yahoo.com>
2022-09-29 19:20:46 +0300

    Using Firebase to save images

--- migrations/20220913092959-create-users.js
+++ migrations/20220913092959-create-users.js
@@ -27,6 +27,9 @@ module.exports = {
       password: {
         type: Sequelize.STRING
       },
+      photo: {
+        type: Sequelize.STRING
+      },
       roleId: {
         type: Sequelize.INTEGER,
         references: {
