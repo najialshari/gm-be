de440f3cf09d719e7dfe176355f8afe449da315c
najialshari <najigoon2@gmail.com>
2022-09-29 18:42:55 +0300

    AuthenticationApplied

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,14 @@ var menusRouter = require('../lib/menu/routes');
 var qrCodesRouter = require('../lib/qrCodes/routes');
 var ordersRouter = require('../lib/orders/routes');
 var subscriptionRouter = require('../lib/subscribers/routes');
+const { isAuthenticated, isAdmin } = require('../lib/middleware');
 
 router.get('/', (req, res, next) => {
     res.send("Welcome to back-end home page")
 });
 router.use('/users', usersRouter);
-router.use('/qrCodes', qrCodesRouter);
-router.use('/orders', ordersRouter);
+router.use('/qrCodes', isAuthenticated , isAdmin, qrCodesRouter);
+router.use('/orders', isAuthenticated , isAdmin, ordersRouter);
 router.use('/menus', menusRouter);
 router.use('/subscription', subscriptionRouter);
 
