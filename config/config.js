7fc8b2d610e8e0a0383ab59611b130c74c300f5b
Naji AlShari <najigoon2@gmail.com>
2023-11-28 17:34:11 +0300

    config multi database using .env file

--- config/config.js
+++ config/config.js
@@ -1,21 +1,13 @@
 require('dotenv').config();
 
 module.exports = {
-  // development: {
-  //   username: process.env.DDB_USERNAME,
-  //   password: process.env.DDB_PASSWORD,
-  //   database: process.env.DDB_NAME,
-  //   host: process.env.DDB_HOST,
-  //   dialect: 'mysql',
-  //   port: 3306,
-  // },
   development: {
-    username: process.env.PGDDB_USERNAME,
-    password: process.env.PGDDB_PASSWORD,
-    database: process.env.PGDDB_NAME,
-    host: process.env.PGDDB_HOST,
-    dialect: 'postgres',
-    port: 5432,
+    username: process.env.DDB_USERNAME,
+    password: process.env.DDB_PASSWORD,
+    database: process.env.DDB_NAME,
+    host: process.env.DDB_HOST,
+    dialect: process.env.DDB_DIALECT,
+    port: process.env.DDB_PORT,
   },
   test: {
     username: process.env.DDB_USERNAME,
